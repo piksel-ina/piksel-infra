0ebff5b0850f020d5224a262910b5aa58c1f7783
Muhammad Taufik <taufik.shf@gmail.com>
2025-05-14 18:33:58 +0700

    Add OIDC-setup configuration for terraform

---
+++ tfstacks-oidc/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-3"
+}
+
+variable "tfc_organization" {
+  type    = string
+  default = "piksel-ina"
+}
+
+variable "tfc_project" {
+  type    = string
+  default = "piksel-ina"
+}
+
+variable "tfc_hostname" {
+  type    = string
+  default = "app.terraform.io"
+}
