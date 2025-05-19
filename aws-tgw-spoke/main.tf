dba00d948709fde42007d1f3bab26cd75ef109c2
taufik-shf <taufik.shf@gmail.com>
2025-05-19 15:51:06 +0700

    Add config to associate all routetables to tgw

--- aws-tgw-spoke/main.tf
+++ aws-tgw-spoke/main.tf
@@ -20,11 +20,15 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "spoke_to_shared_tgw" {
   })
 }
 
+data "aws_route_tables" "vpc_route_tables" {
+  vpc_id = var.vpc_id
+}
+
 # -- Add route to spoke-VPC's route table, it directs traffic to hub CIDR via TGW --
 resource "aws_route" "spoke_to_shared_vpc_via_tgw" {
-  count = length(var.spoke_vpc_route_table_id)
+  count = length(data.aws_route_tables.vpc_route_tables.ids)
 
-  route_table_id         = var.spoke_vpc_route_table_id[count.index]
+  route_table_id         = data.aws_route_tables.vpc_route_tables.ids[count.index]
   destination_cidr_block = var.vpc_cidr_shared
   transit_gateway_id     = var.transit_gateway_id
 
