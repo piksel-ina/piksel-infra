774275beb54bc107842b6dfea5e826059dc43c91
taufik-shf <taufik.shf@gmail.com>
2026-04-24 19:07:03 +0700

    Add required_providers to aws-s3-static-hosting module

--- aws-s3-static-hosting/variables.tf
+++ aws-s3-static-hosting/variables.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
 variable "project" {
   description = "Project name used for resource naming and tagging"
   type        = string
